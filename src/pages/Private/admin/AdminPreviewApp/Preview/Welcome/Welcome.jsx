8b2f6686157bd9e53d8338d6a422de8a62182b44
RFID-NC <stoyann.noumea@outlook.com>
2023-08-20 23:44:21 +1100

    cover welcome

--- src/pages/Private/admin/AdminPreviewApp/Preview/Welcome/Welcome.jsx
+++ src/pages/Private/admin/AdminPreviewApp/Preview/Welcome/Welcome.jsx
@@ -3,14 +3,49 @@ import HeaderSection from '../HeaderSection/HeaderSection'
 import hotelBanner from '../../../../../../assets/hotel_banner.jpeg'
 function Welcome() {
   return (
-    <div>
-        
+    <>
         <HeaderSection title={'Bienvenue !'}/>
+    <div className='mainPrevContainer'>
+        
 
         <div className="cover-homePreviewContainer">
             <img src={hotelBanner} alt="" />
+            <div className="div">
+              
+            Au cœur de la cité, le Bellevue est heureux de vous accueillir dans un site historique privilégié. Vous pouvez retrouver ici toutes les informations utiles et recommandations qui feront de votre séjour un moment inoubliable. Toute l'équipe est à votre disposition pour vous offrir tout le confort souhaité dans une atmosphère unique et chaleureuse.
+            </div>
         </div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
+        <div>TEST</div>
     </div>
+    </>
   )
 }
 
