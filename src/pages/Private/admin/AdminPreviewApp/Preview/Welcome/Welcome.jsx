c8d0118a22550d16a47e15177c1418e332ae94af
RFID-NC <stoyann.noumea@outlook.com>
2023-08-20 23:30:25 +1100

    styling prev sidebar

--- src/pages/Private/admin/AdminPreviewApp/Preview/Welcome/Welcome.jsx
+++ src/pages/Private/admin/AdminPreviewApp/Preview/Welcome/Welcome.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import HeaderSection from '../HeaderSection/HeaderSection'
-
+import hotelBanner from '../../../../../../assets/hotel_banner.jpeg'
 function Welcome() {
   return (
     <div>
         
         <HeaderSection title={'Bienvenue !'}/>
+
+        <div className="cover-homePreviewContainer">
+            <img src={hotelBanner} alt="" />
+        </div>
     </div>
   )
 }
